ada02e632c57c48f9114503e79c7dcf86e7d4a28
LucasGomes <111081582+Glomes@users.noreply.github.com>
2023-08-07 15:05:17 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projectWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TBShow(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 0.5;
+        }
+        private void TBHide(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 1;
+        }
+        private void PMLBDow(object sender, MouseButtonEventArgs e)
+        {
+            ShowHide.IsChecked = false;
+        }
+
+
+        private void PreviewMouseLeftBottonDownBG(object sender, MouseButtonEventArgs e) {
+            ShowHide.IsChecked = false;
+        }
+
+        private void Minimizar(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Fechar(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
